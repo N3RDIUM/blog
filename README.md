fdab73f7527061d9133478792408976162b4677e
1\13121)11_11\/1 <n3rdium@gmail.com>
2026-03-01 10:53:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a template for parts of my website that I'm organizing into separate
+git repos. Why? Because I want to work with Obsidian but don't want to clutter
+up the main website repo's build system, and I want something modular.
+
+Why not use Obsidian Publish? Well, too late for that, I guess!
+
+Feel free to check out how the `example.md` is rendered
+[here](https://n3rdium.dev/mdsite-template-test/example.html). The link may not
+work if the 
+[overhaul branch](https://github.com/N3RDIUM/n3rdium.dev/tree/overhaul) isn't
+merged yet.
---
+++ scripts/dist.py
@@ -0,0 +1,112 @@
+import json
+import os
+import shutil
+import markdown
+
+from .metadata import parse_metadata, trim_metadata
+
+if os.path.exists("./dist/"):
+    shutil.rmtree("./dist/")
+os.mkdir("dist")
+
+# TODO preprocessor that transforms wikilinks to html paths
+# should intelligently handle internal links (within vault) and external ones
+
+def md_to_html(path: str) -> tuple[str, dict]:
+    with open(path, "r", encoding="utf-8") as f:
+        md = f.read()
+
+    html = markdown.markdown(
+        trim_metadata(md),
+        extensions=[
+            "extra",
+            "toc",
+            "codehilite",
+            "mdx_wikilink_plus",
+            "pymdownx.arithmatex"
+        ]
+    )
+
+    metadata = parse_metadata(md)
+
+    return html, metadata
+
+DEFAULT_METADATA: dict[str, str] = {
+    "title": "Lorem Ipsum",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+}
+
+def apply_template(content: str, metadata: dict) -> str:
+    title = metadata.get("title", DEFAULT_METADATA["title"])
+    description = metadata.get("description", DEFAULT_METADATA["description"])
+
+    return f"""<!--meta start
+{json.dumps(metadata)}
+meta end-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!--metadata-->
+        <meta charset="utf-8">
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="{description}">
+
+        <!--inline start
+            css:theme
+            css:default
+            css:mathjax
+            js:mathjax_config
+        inline end-->
+        <!--autogen start-->
+        <!--autogen end-->
+        <script async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
+    </head>
+    <body>
+        <main class="container">
+{content}
+        </main>
+    </body>
+</html>
+"""
+
+SRC_DIR = "vault/"
+
+def process_md_file(root: str, file: str):
+    path: str = os.path.join(root, file)
+    dist_path: str = os.path.join(
+        "dist/",
+        path.removeprefix(SRC_DIR).removesuffix(".md") + ".html"
+    )
+    dist_dir: str = os.path.dirname(dist_path)
+
+    html, metadata = md_to_html(path)
+    print(f"{path} -> {dist_path}")
+    os.makedirs(dist_dir, exist_ok=True)
+    with open(dist_path, "w") as f:
+        _ = f.write(apply_template(html, metadata))
+
+def direct_copy(root: str, file: str):
+    path: str = os.path.join(root, file)
+    dist_path: str = os.path.join(
+        "dist/",
+        path.removeprefix(SRC_DIR)
+    )
+    dist_dir: str = os.path.dirname(dist_path)
+    print(f"{path} -> {dist_path}")
+    os.makedirs(dist_dir, exist_ok=True)
+    _ = shutil.copy(path, dist_path)
+
+if __name__ == "__main__":
+    for root, _, files in os.walk(SRC_DIR, topdown=True):
+        if ".obsidian" in root:
+            continue
+        if ".trash" in root:
+            continue
+
+        for file in files:
+            if not file.endswith(".md"):
+                direct_copy(root, file)
+                continue
+            process_md_file(root, file)
